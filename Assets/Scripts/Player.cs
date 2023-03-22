fd5894338f12fbf5c52d18fec48b6f7933ba7076
reberaidan <aidanweinreber@hotmail.com>
2023-03-22 04:34:09 -0500

    Movement Basics
    I implemented the new input system into the project. We are able to walk, jump, and crouch. Tried to start fluttering but could not figure it out at this time. Additionally attached the following camera to the player.
    
    If the player moves to the side of a platform, they will stick to it, which is not intended. Will likely need to shrink the ceiling and grounded hit boxes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public PlayerStuff playerControls;
+
+    private float move;
+    private CharacterController2D charCon;
+    private InputAction movement;
+    private InputAction jumping;
+    private InputAction crouching;
+    private InputAction fluttering;
+    private bool jump;
+    private bool crouch;
+    private bool flutter;
+
+    private void Awake()
+    {
+        playerControls = new PlayerStuff();
+    }
+
+    private void OnEnable()
+    {
+        movement = playerControls.Player.move;
+        jumping = playerControls.Player.jump;
+        crouching = playerControls.Player.crouch;
+        fluttering = playerControls.Player.flutter;
+        movement.Enable();
+        jumping.Enable();
+        crouching.Enable();
+        fluttering.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        jumping.Disable();
+        crouching.Disable();
+        fluttering.Disable();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        charCon = GetComponent<CharacterController2D>();
+        jump = false;
+        crouch = false;
+        flutter = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        move = movement.ReadValue<float>();
+        jump = jumping.ReadValue<float>() > 0.1f;
+        crouch = crouching.ReadValue<float>() > 0.1f;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        charCon.Move(move, crouch, jump,flutter);
         
     }
 }
