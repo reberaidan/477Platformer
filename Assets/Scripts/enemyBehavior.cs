de1cf2239c6547f507392184559c0014a590f127
reberaidan <aidanweinreber@hotmail.com>
2023-03-22 14:15:06 -0500

    Enemy Basics
    Added basic back and forth enemies. For now they are just represented by  circles. They are able to bounce off of each other and walls that have tag: wall.  Need to add condition for if the player gets hit by them and if we want to kill enemies

---
+++ Assets/Scripts/enemyBehavior.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehavior : MonoBehaviour
+{
+    
+    private Rigidbody2D m_Rigidbody2D;
+    public float move = 0.1f;
+    private float m_MovementSmoothing = 0.2f;
+    private Vector3 m_Velocity = Vector3.zero;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_Rigidbody2D.velocity.x == 0)
+        {
+            m_Rigidbody2D.velocity = new Vector2(move,m_Rigidbody2D.velocity.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+        // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("wall"))
+        {
+            move *= -1;
+        }
+        else if (collision.transform.CompareTag("enemy"))
+        {
+            move *= -1;
+        }
+    }
+
+    
+}
