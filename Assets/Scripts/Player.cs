5946d87f7ee5d01a734035a84f4afbf9af6602c1
Dillan Mickel <80413966+DillanPickle001@users.noreply.github.com>
2023-03-27 10:19:54 -0500

    Added a bit more to Castle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour
     private bool crouch;
     private bool flutter;
 	private bool sjump;
-	private bool slam;
+	public bool slam;
 
     private void Awake()
     {
